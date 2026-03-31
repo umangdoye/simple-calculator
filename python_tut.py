d8e3a33a77cb1494b5d682e581bb432af61172e9
umangdoye <umangdoye@gmail.com>
2026-03-31 10:04:39 +0530

    Added my first Python program

---
+++ python_tut.py
@@ -0,0 +1,17 @@
+import sys
+print(sys.version)
+
+print("My name is Umang")  # print your name
+print("I am", 20)  # print your age
+print("My hobby is to play Basketball")  # hobby
+
+#taking inputs
+name = str(input("What is your name: "))
+
+age = int(input("What is your age: "))
+
+hobby = str(input("What is your hobby: "))
+
+print("Name:", name)
+print("age: ", age)
+print("hobby: ", hobby)
