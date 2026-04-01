5bd1c6f27733ec9c67bb28bc9f8f4f83487d6285
umangdoye <umangdoye@gmail.com>
2026-04-01 22:08:01 +0530

    Added simple calculator program

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# simple calculator
+a = float(input("enter first number:"))
+b = float(input("enter second number:"))
+
+print("addition of numbers is: ", a+b)
+print("difference of two numbers is: ", a-b)
+print("product of two numbers is: ", a*b)
+print("quotient of two numbers is: ", a/b)
